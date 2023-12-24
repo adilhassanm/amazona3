edb8ad1008237411b443bc560a9d1e73ddb6ab11
Adil <adil27jan@gmail.com>
2023-12-24 14:14:38 +0400

    creae react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>{' '}
+        {/* a is anchor to root of application with / */}
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
